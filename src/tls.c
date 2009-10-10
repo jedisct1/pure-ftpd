a8bee0435ebd8fdeabe2f1b354aceb9c811f5ba2
Frank Denis (Jedi/Sector One) <github@pureftpd.org>
2009-10-10 18:51:28 +0200

    Handle DH keys for ephemeral key exchange.

--- src/tls.c
+++ src/tls.c
@@ -32,43 +32,133 @@ static void tls_error(const int line, int err)
     _EXIT(EXIT_FAILURE);
 }
 
+DH *get_dh512(void)
+{
+	static unsigned char dh512_p[]={
+		0xF5,0x2A,0xFF,0x3C,0xE1,0xB1,0x29,0x40,0x18,0x11,0x8D,0x7C,
+		0x84,0xA7,0x0A,0x72,0xD6,0x86,0xC4,0x03,0x19,0xC8,0x07,0x29,
+		0x7A,0xCA,0x95,0x0C,0xD9,0x96,0x9F,0xAB,0xD0,0x0A,0x50,0x9B,
+		0x02,0x46,0xD3,0x08,0x3D,0x66,0xA4,0x5D,0x41,0x9F,0x9C,0x7C,
+		0xBD,0x89,0x4B,0x22,0x19,0x26,0xBA,0xAB,0xA2,0x5E,0xC3,0x55,
+		0xE9,0x2A,0x05,0x5F,
+    };
+	static unsigned char dh512_g[]={
+		0x02,
+    };
+	DH *dh;
+
+	if ((dh=DH_new()) == NULL) return(NULL);
+	dh->p=BN_bin2bn(dh512_p,sizeof(dh512_p),NULL);
+	dh->g=BN_bin2bn(dh512_g,sizeof(dh512_g),NULL);
+	if ((dh->p == NULL) || (dh->g == NULL))
+    { DH_free(dh); return(NULL); }
+	return(dh);
+}
+
+DH *get_dh1024(void)
+{
+	static unsigned char dh1024_p[]={
+		0xF4,0x88,0xFD,0x58,0x4E,0x49,0xDB,0xCD,0x20,0xB4,0x9D,0xE4,
+		0x91,0x07,0x36,0x6B,0x33,0x6C,0x38,0x0D,0x45,0x1D,0x0F,0x7C,
+		0x88,0xB3,0x1C,0x7C,0x5B,0x2D,0x8E,0xF6,0xF3,0xC9,0x23,0xC0,
+		0x43,0xF0,0xA5,0x5B,0x18,0x8D,0x8E,0xBB,0x55,0x8C,0xB8,0x5D,
+		0x38,0xD3,0x34,0xFD,0x7C,0x17,0x57,0x43,0xA3,0x1D,0x18,0x6C,
+		0xDE,0x33,0x21,0x2C,0xB5,0x2A,0xFF,0x3C,0xE1,0xB1,0x29,0x40,
+		0x18,0x11,0x8D,0x7C,0x84,0xA7,0x0A,0x72,0xD6,0x86,0xC4,0x03,
+		0x19,0xC8,0x07,0x29,0x7A,0xCA,0x95,0x0C,0xD9,0x96,0x9F,0xAB,
+		0xD0,0x0A,0x50,0x9B,0x02,0x46,0xD3,0x08,0x3D,0x66,0xA4,0x5D,
+		0x41,0x9F,0x9C,0x7C,0xBD,0x89,0x4B,0x22,0x19,0x26,0xBA,0xAB,
+		0xA2,0x5E,0xC3,0x55,0xE9,0x2F,0x78,0xC7,
+    };
+	static unsigned char dh1024_g[]={
+		0x02,
+    };
+	DH *dh;
+
+	if ((dh=DH_new()) == NULL) return(NULL);
+	dh->p=BN_bin2bn(dh1024_p,sizeof(dh1024_p),NULL);
+	dh->g=BN_bin2bn(dh1024_g,sizeof(dh1024_g),NULL);
+	if ((dh->p == NULL) || (dh->g == NULL))
+    { DH_free(dh); return(NULL); }
+	return(dh);
+}
+
+static DH *cb_tmp_dh(SSL * const ctx, const int for_export,
+                     const int key_length)
+{
+    (void) ctx;
+    if (for_export == 0 || key_length >= 1024) {
+        return get_dh1024();
+    }
+    return get_dh512();
+}
+
 static int tls_init_diffie(void)
 {
-    DH *dh;
-    BIO *bio;
+    DH *dh = NULL;
+    BIO *bio = NULL;
+    int ret = 0;
 
     if ((bio = BIO_new_file(TLS_CERTIFICATE_FILE, "r")) == NULL) {
         logfile(LOG_ERR, "SSL/TLS: Can't read [%s]",
                 TLS_CERTIFICATE_FILE);
-        return -1;
+        ret = -1;
+        goto end;
     }
     if ((dh = PEM_read_bio_DHparams(bio, NULL, NULL
 # if OPENSSL_VERSION_NUMBER >= 0x00904000L
                                     , NULL
 # endif
                                     )) == NULL) {
-        BIO_free(bio);
-        logfile(LOG_DEBUG, "SSL/TLS: Can't read DH parameters");
-        return 1;
+        ret = 1;
+        goto end;
     }
     if (SSL_CTX_set_tmp_dh(tls_ctx, dh) != 1) {
         logfile(LOG_ERR, "SSL/TLS: Can't set ephemeral keys");
-        return -1;
+        ret = -1;
     }
-    DH_free(dh);    
-    BIO_free(bio);
-
+    end:
+    if (dh != NULL) {
+        DH_free(dh);
+    }
+    if (bio != NULL) {
+        BIO_free(bio);
+    }    
+    if (ret != 0) {
+        SSL_CTX_set_tmp_dh_callback(tls_ctx, cb_tmp_dh);
+    }
     return 0;
+}
+
+static RSA *get_rsa(const unsigned long key_length)
+{
+    return RSA_generate_key(key_length, RSA_F4, NULL, NULL);
+}
+
+static RSA *cb_tmp_rsa(SSL * const ctx,
+                       const int for_export, const int key_length)
+
+{
+    (void) ctx;
+    if (for_export == 0 || key_length >= 1024) {
+        return get_rsa(1024);
+    }
+    return get_rsa(512);
 }
 
 static void tls_init_cache(void)
 {
+    static const char *tls_ctx_id = "pure-ftpd";
+    
     SSL_CTX_set_session_cache_mode(tls_ctx, SSL_SESS_CACHE_SERVER);
+    SSL_CTX_set_session_id_context(tls_ctx, (unsigned char *) tls_ctx_id,
+                                   (unsigned int) strlen(tls_ctx_id));
+    SSL_CTX_sess_set_cache_size(tls_ctx, 10);
+    SSL_CTX_set_timeout(tls_ctx, 60 * 60L);
 }
 
 int tls_init_library(void) 
 {
-    const char *tls_ctx_id = "pure-ftpd";    
     unsigned int rnd;
     
     SSL_library_init();
@@ -80,35 +170,29 @@ int tls_init_library(void)
     if ((tls_ctx = SSL_CTX_new(SSLv23_server_method())) == NULL) {
         tls_error(__LINE__, 0);
     }
-    tls_init_cache();
-    SSL_CTX_set_options(tls_ctx, SSL_OP_ALL);    
+    SSL_CTX_set_options(tls_ctx, SSL_OP_NO_SSLv2 | SSL_OP_ALL
+# ifdef SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION
+                        | SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION
+# endif
+                        );
+
     if (SSL_CTX_use_certificate_chain_file(tls_ctx,
                                            TLS_CERTIFICATE_FILE) != 1) {
         die(421, LOG_ERR,
             MSG_FILE_DOESNT_EXIST ": [%s]", TLS_CERTIFICATE_FILE);
     }
     if (SSL_CTX_use_PrivateKey_file(tls_ctx, TLS_CERTIFICATE_FILE,
-                                    X509_FILETYPE_PEM) != 1) {
+                                    SSL_FILETYPE_PEM) != 1) {
         tls_error(__LINE__, 0);
     }
     if (SSL_CTX_check_private_key(tls_ctx) != 1) {
         tls_error(__LINE__, 0);
-    }
-    if (SSL_CTX_need_tmp_RSA(tls_ctx)) {
-        RSA *ephemeral_key;
-
-        if ((ephemeral_key = RSA_generate_key(RSA_EPHEMERAL_KEY_LEN, 
-                                              RSA_F4, NULL, NULL)) == NULL) {
-            tls_error(__LINE__, 0);
-        }
-        if (SSL_CTX_set_tmp_rsa(tls_ctx, ephemeral_key) != 1) {
-            tls_error(__LINE__, 0);
-        }
-        RSA_free(ephemeral_key);
-    }
+    }    
+    SSL_CTX_set_tmp_rsa_callback(tls_ctx, cb_tmp_rsa);
     if (tls_init_diffie() < 0) {
         tls_error(__LINE__, 0);
     }
+    tls_init_cache();
 #ifdef REQUIRE_VALID_CLIENT_CERTIFICATE
     SSL_CTX_set_verify(tls_ctx, SSL_VERIFY_FAIL_IF_NO_PEER_CERT |
                        SSL_VERIFY_PEER | SSL_VERIFY_CLIENT_ONCE, NULL);
@@ -116,10 +200,7 @@ int tls_init_library(void)
                                       TLS_CERTIFICATE_FILE, NULL) != 0) {
         tls_error(__LINE__, 0);
     }
-#endif
-    SSL_CTX_set_session_id_context(tls_ctx, (unsigned char *) tls_ctx_id,
-                                   (unsigned int) strlen(tls_ctx_id));
-    
+#endif    
     return 0;
 }
 
@@ -202,7 +283,6 @@ int tls_init_data_session(const int fd, const int passive)
     } else {
         SSL_set_connect_state(tls_data_cnx);
     }
-#if 1
     for (;;) {
         if ((ret = SSL_accept(tls_data_cnx)) <= 0) {
             ret_ = SSL_get_error(tls_data_cnx, ret);
@@ -214,7 +294,6 @@ int tls_init_data_session(const int fd, const int passive)
         }
         break;
     }
-#endif
 #if ONLY_ACCEPT_REUSED_SSL_SESSIONS
     if (SSL_session_reused(tls_data_cnx) == 0) {
         tls_error(__LINE__, 0);
