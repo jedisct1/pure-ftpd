3a20b04d443a66062911f4a7c145e9e285cc0477
Frank Denis (Jedi/Sector One) <github@pureftpd.org>
2009-10-31 01:15:32 +0100

    Add -pthread to the iPhone compile flags. It can't hurt.

--- iphone/compile-iphoneOS.sh
+++ iphone/compile-iphoneOS.sh
@@ -6,8 +6,8 @@ export PLATFORM_PATH="/Developer/Platforms/iPhoneOS.platform"
 export TARGET_PATH="$PLATFORM_PATH/Developer/SDKs/iPhoneOS3.1.sdk"
 export CC="$PLATFORM_PATH/Developer/usr/bin/gcc"
 export CPPFLAGS="-D__IPHONE__=1 -DNO_PROCNAME_CHANGE -DANON_CAN_CHANGE_PERMS=1 -DANON_CAN_CHANGE_UTIME=1 -DANON_CAN_DELETE=1 -DANON_CAN_RESUME=1 -DANON_CAN_RENAME=1 -I$TARGET_PATH/usr/include -I$TARGET_PATH/usr/lib/gcc/arm-apple-darwin9/4.2.1/include"
-export CFLAGS="-Oz -arch armv5 -arch armv6 -arch armv7"
-export LDFLAGS="-L$TARGET_PATH/usr/lib -arch armv5 -arch armv6 -arch armv7"
+export CFLAGS="-pthread -Oz -arch armv5 -arch armv6 -arch armv7"
+export LDFLAGS="-pthread -L$TARGET_PATH/usr/lib -arch armv5 -arch armv6 -arch armv7"
 export CPP="$PLATFORM_PATH/Developer/usr/bin/cpp"
 
 rm -fr obj-arm 2>/dev/null
@@ -21,8 +21,8 @@ mv libpureftpd.a _libpureftpd-armv6.a
 
 (
 export CPPFLAGS="-D__IPHONE__=1 -DNO_PROCNAME_CHANGE -DANON_CAN_CHANGE_PERMS=1 -DANON_CAN_CHANGE_UTIME=1 -DANON_CAN_DELETE=1 -DANON_CAN_RESUME=1 -DANON_CAN_RENAME=1"
-export CFLAGS="-Os -arch i386 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5"
-export LDFLAGS="-arch i386 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5"
+export CFLAGS="-pthread -Os -arch i386 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5"
+export LDFLAGS="-pthread -arch i386 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5"
 
 rm -fr obj-i386 2>/dev/null
 mkdir obj-i386 2>/dev/null
@@ -35,8 +35,8 @@ mv libpureftpd.a _libpureftpd-i386.a
 
 (
 export CPPFLAGS="-D__IPHONE__=1 -DNO_PROCNAME_CHANGE -DANON_CAN_CHANGE_PERMS=1 -DANON_CAN_CHANGE_UTIME=1 -DANON_CAN_DELETE=1 -DANON_CAN_RESUME=1 -DANON_CAN_RENAME=1"
-export CFLAGS="-Os -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5"
-export LDFLAGS="-arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5"
+export CFLAGS="-pthread -Os -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5"
+export LDFLAGS="-pthread -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5"
 
 rm -fr obj-x86_64 2>/dev/null
 mkdir obj-x86_64 2>/dev/null
