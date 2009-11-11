e83f3552922a223dda0162ec4576061dd90e5e23
Frank Denis (Jedi/Sector One) <github@pureftpd.org>
2009-11-11 10:05:49 +0100

    Reduce MMAP_SIZE further

--- src/ftpd.h
+++ src/ftpd.h
@@ -567,10 +567,10 @@ Your platform has a very large MAXPATHLEN, we should not trust it.
 # define DL_DEFAULT_CHUNK_SIZE_ASCII 32768UL
 #endif
 #ifndef DL_MAX_CHUNK_SIZE
-# define DL_MAX_CHUNK_SIZE (512 * 1024UL)
+# define DL_MAX_CHUNK_SIZE (128 * 1024UL)
 #endif
 #ifndef DL_MMAP_SIZE
-# define DL_MMAP_SIZE (512 * 1024UL)
+# define DL_MMAP_SIZE (128 * 1024UL)
 #endif
 #if DL_MMAP_SIZE < DL_MAX_CHUNK_SIZE
 # error DL_MMAP_SIZE should be >= DL_MAX_CHUNK_SIZE
