f5b35ec59c0bccdfe81ef264850075ed313b14d8
Frank DENIS <git@pureftpd.org>
2010-01-23 23:27:47 +0100

    POLLIN = POLLRDNORM | POLLRDBAND

--- src/ftpd.c
+++ src/ftpd.c
@@ -2533,7 +2533,7 @@ void opendata(void)
                 (pfds[1].revents & (POLLERR | POLLHUP | POLLNVAL)) != 0) {
                 die(221, LOG_INFO, MSG_LOGOUT);
             }
-            if ((pfds[1].revents & (POLLIN | POLLRDNORM)) == 0) {
+            if ((pfds[1].revents & POLLIN) == 0) {
                 continue;
             }
             socksize = (socklen_t) sizeof(dataconn);
@@ -3279,8 +3279,7 @@ int dlhandler_handle_commands(DLHandler * const dlhandler,
     if (pollret <= 0) {
         return pollret;
     }
-    if ((dlhandler->pfds_f_in.revents &
-         (POLLIN | POLLPRI | POLLRDBAND | POLLRDNORM)) != 0) {
+    if ((dlhandler->pfds_f_in.revents & (POLLIN | POLLPRI)) != 0) {
         if (dlhandler->tls_clientfd != NULL) {
 #ifdef WITH_TLS            
             readnb = SSL_read(dlhandler->tls_clientfd, buf,
@@ -4100,8 +4099,7 @@ int ul_handle_data(ULHandler * const ulhandler, off_t * const uploaded,
                  (POLLERR | POLLHUP | POLLNVAL)) != 0) {
                 return -1;
             }
-            if ((ulhandler->pfds_command.revents &
-                 (POLLIN | POLLPRI | POLLRDBAND | POLLRDNORM)) != 0) {
+            if ((ulhandler->pfds_command.revents & (POLLIN | POLLPRI)) != 0) {
                 ret = ulhandler_handle_commands(ulhandler);
                 if (ret != 0) {
                     return ret;
@@ -4144,7 +4142,7 @@ int ul_send(ULHandler * const ulhandler)
             addreply_noformat(421, MSG_TIMEOUT);
             return -1;
         }
-        if ((ulhandler->pfds[PFD_DATA].revents & (POLLIN | POLLRDNORM)) != 0) {
+        if ((ulhandler->pfds[PFD_DATA].revents & POLLIN) != 0) {
             ret = ul_handle_data(ulhandler, &uploaded, ts_start);
             switch (ret) {
             case 1:
@@ -4160,8 +4158,7 @@ int ul_send(ULHandler * const ulhandler)
                 return ret;
             }
         }
-        if ((ulhandler->pfds[PFD_COMMANDS].revents &
-             (POLLIN | POLLPRI | POLLRDBAND | POLLRDNORM)) != 0) {
+        if ((ulhandler->pfds[PFD_COMMANDS].revents & (POLLIN | POLLPRI)) != 0) {
             ret = ulhandler_handle_commands(ulhandler);
             if (ret != 0) {
                 return ret;
@@ -4169,8 +4166,7 @@ int ul_send(ULHandler * const ulhandler)
         }
         if ((ulhandler->pfds[PFD_DATA].revents & (POLLERR | POLLNVAL)) != 0 ||
             ((ulhandler->pfds[PFD_DATA].revents & POLLHUP) != 0 &&
-             (ulhandler->pfds[PFD_DATA].revents &
-              (POLLIN | POLLRDNORM)) == 0)) {
+             (ulhandler->pfds[PFD_DATA].revents & POLLIN) == 0)) {
             return -1;
         }
         if ((ulhandler->pfds[PFD_COMMANDS].revents &
