232c5e97b4b6582646c0b830ab89309294415182
Frank Denis <github@pureftpd.org>
2011-10-13 20:26:48 -0700

    Unbreak compilation on Linux.

--- src/bsd-glob.c
+++ src/bsd-glob.c
@@ -1008,7 +1008,8 @@ g_opendir(Char *str, glob_t *pglob)
 	char buf[MAXPATHLEN];
 
 	if (!*str) {
-		strlcpy(buf, ".", sizeof buf);
+		buf[0] = '.';
+		buf[1] = 0;
 	} else {
 		if (g_Ctoc(str, buf, sizeof(buf))) {
 			return NULL;
