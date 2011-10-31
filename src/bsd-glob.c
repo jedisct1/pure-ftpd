e0643f9f3c53d5c5ffaebc421cb44731d9c38dcf
Frank Denis <github@pureftpd.org>
2011-10-30 18:56:55 -0700

    Unfortunately Linux still hasn't strlcpy()

--- src/bsd-glob.c
+++ src/bsd-glob.c
@@ -880,7 +880,8 @@ g_opendir(Char *str, glob_t *pglob)
     char buf[MAXPATHLEN];
 
     if (!*str) {
-        strlcpy(buf, ".", sizeof buf);
+        buf[0] = '.';
+        buf[1] = 0;
     } else {
         if (g_Ctoc(str, buf, sizeof(buf))) {
             return NULL;
