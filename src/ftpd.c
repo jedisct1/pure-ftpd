3c9ca0c77c92fafbe40d58146855bf65d74265cd
Frank Denis <github@pureftpd.org>
2011-12-15 09:12:42 -0800

    Initialize TZ even if strftime() fails.

--- src/ftpd.c
+++ src/ftpd.c
@@ -163,16 +163,16 @@ static int init_tz(void)
 #ifdef HAVE_TZSET
     tzset();
 #endif
-#ifdef HAVE_PUTENV    
-    if ((tm = localtime(&now)) == NULL ||
-        strftime(stbuf, sizeof stbuf, "%z", tm) != (size_t) 5U) {
-        return -1;
+#ifdef HAVE_PUTENV
+    if ((tm = localtime(&now)) != NULL &&
+        strftime(stbuf, sizeof stbuf, "%z", tm) == (size_t) 5U) {
+        snprintf(default_tz_for_putenv, sizeof default_tz_for_putenv,
+                 "TZ=UTC%c%c%c:%c%c", (*stbuf == '-' ? '+' : '-'),
+                 stbuf[1], stbuf[2], stbuf[3], stbuf[4]);
     }
-    snprintf(default_tz_for_putenv, sizeof default_tz_for_putenv,
-             "TZ=UTC%c%c%c:%c%c", (*stbuf == '-' ? '+' : '-'),
-             stbuf[1], stbuf[2], stbuf[3], stbuf[4]);
     putenv(default_tz_for_putenv);
 #endif
+    (void) localtime(&now);
     (void) gmtime(&now);
 
     return 0;
