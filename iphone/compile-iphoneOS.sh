fa848c3d3879d89a7c257f7bbe6ba5f6492ab912
Frank Denis <github@pureftpd.org>
2012-10-07 20:27:47 -0700

    Rework the compile-iPhoneOS.sh script.

--- iphone/compile-iphoneOS.sh
+++ iphone/compile-iphoneOS.sh
@@ -1,38 +1,38 @@
 #! /bin/sh
 
+export CPPFLAGS="-D__IPHONE__=1 -DALLOW_DELETION_OF_TEMPORARY_FILES=1 -DNO_PROCNAME_CHANGE -DANON_CAN_CHANGE_PERMS=1 -DANON_CAN_CHANGE_UTIME=1 -DANON_CAN_DELETE=1 -DANON_CAN_RESUME=1 -DANON_CAN_RENAME=1"
+export XCODEDIR="/Applications/Xcode.app/Contents/Developer"
+
 for arch in armv7 armv7s; do
 (
-export XCODE_PATH="/Applications/Xcode.app/Contents"
-export PATH="$XCODE_PATH/Developer/usr/bin:$XCODE_PATH/Developer/usr/sbin:$PATH"
-export PLATFORM_PATH="$XCODE_PATH/Developer/Platforms/iPhoneOS.platform"
-export TARGET_PATH="$PLATFORM_PATH/Developer/SDKs/iPhoneOS6.0.sdk"
-export CC="$PLATFORM_PATH/Developer/usr/bin/gcc"
-export CPPFLAGS="-D__IPHONE__=1 -DALLOW_DELETION_OF_TEMPORARY_FILES=1 -DNO_PROCNAME_CHANGE -DANON_CAN_CHANGE_PERMS=1 -DANON_CAN_CHANGE_UTIME=1 -DANON_CAN_DELETE=1 -DANON_CAN_RESUME=1 -DANON_CAN_RENAME=1 -I$TARGET_PATH/usr/include -I$TARGET_PATH/usr/lib/gcc/arm-apple-darwin10/4.2.1/include"
-export CFLAGS="-pthread -Oz -arch $arch"
-export LDFLAGS="-pthread -L$TARGET_PATH/usr/lib -L$TARGET_PATH/usr/lib/system -arch $arch"
-export CPP="$PLATFORM_PATH/Developer/usr/bin/llvm-cpp-4.2"
+export BASEDIR="${XCODEDIR}/Platforms/iPhoneOS.platform/Developer"
+export PATH="${BASEDIR}/usr/bin:$BASEDIR/usr/sbin:$PATH"
+export SDK="${BASEDIR}/SDKs/iPhoneOS6.0.sdk"
+export CFLAGS="-pthread -Oz -mthumb -arch ${arch} -isysroot ${SDK}"
+export LDFLAGS="-pthread -mthumb -arch ${arch} -isysroot ${SDK}"
 
 rm -fr "obj-$arch" 2>/dev/null
 mkdir "obj-$arch" 2>/dev/null
 cd "obj-$arch" || exit 1
-../../configure --host=i686-apple-darwin11 --with-boring --without-inetd --without-pam --with-nonroot --with-rfc2640 || exit 2
+../../configure --host=arm-apple-darwin10 --with-boring --without-inetd --without-pam --with-nonroot --with-rfc2640 || exit 2
 make || make
 cd src || exit 1
 mv libpureftpd.a "_libpureftpd-$arch.a"
 )
 done
 
-for arch in i386 x86_64; do
+for arch in i386; do
 (
-export XCODE_PATH="/Applications/Xcode.app/Contents"
-export CPPFLAGS="-D__IPHONE__=1 -DALLOW_DELETION_OF_TEMPORARY_FILES=1 -DNO_PROCNAME_CHANGE -DANON_CAN_CHANGE_PERMS=1 -DANON_CAN_CHANGE_UTIME=1 -DANON_CAN_DELETE=1 -DANON_CAN_RESUME=1 -DANON_CAN_RENAME=1"
-export CFLAGS="-pthread -Os -arch $arch -isysroot $XCODE_PATH/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk -mmacosx-version-min=10.6"
-export LDFLAGS="-pthread -arch $arch -isysroot $XCODE_PATH/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk -mmacosx-version-min=10.6"
+export BASEDIR="${XCODEDIR}/Platforms/iPhoneSimulator.platform/Developer"
+export PATH="${BASEDIR}/usr/bin:$BASEDIR/usr/sbin:$PATH"
+export SDK="${BASEDIR}/SDKs/iPhoneSimulator6.0.sdk"
+export CFLAGS="-pthread -Oz -mthumb -arch ${arch} -isysroot ${SDK}"
+export LDFLAGS="-pthread -mthumb -arch ${arch} -isysroot ${SDK}"
 
 rm -fr "obj-$arch" 2>/dev/null
 mkdir "obj-$arch" 2>/dev/null
 cd "obj-$arch" || exit 1
-../../configure --with-boring --without-inetd --without-pam --with-nonroot --with-rfc2640 || exit 2
+../../configure --host=arm-apple-darwin10 --with-boring --without-inetd --without-pam --with-nonroot --with-rfc2640 || exit 2
 make || make
 cd src || exit 1
 mv libpureftpd.a "_libpureftpd-$arch.a"
@@ -41,10 +41,8 @@ done
 
 lipo -create -output libpureftpd.a \
   obj-arm*/src/_libpureftpd-armv*.a \
-  obj-i386/src/_libpureftpd-i386.a \
-  obj-x86_64/src/_libpureftpd-x86_64.a || exit 3
+  obj-i386*/src/_libpureftpd-i386.a || exit 3
   
-rm -fr obj-arm* obj-i386 obj-x86_64
+rm -fr obj-arm* obj-i386
 
 lipo -info libpureftpd.a
-
