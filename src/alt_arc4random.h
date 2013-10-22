6e9ec67ae461938caa2c641f7ca617298d5cca08
Frank Denis <github@pureftpd.org>
2013-10-22 13:10:31 -0700

    Check for arc4random_stir and arc4random_addrandom presence

--- src/alt_arc4random.h
+++ src/alt_arc4random.h
@@ -8,8 +8,16 @@
 #if defined(__OpenBSD__) || defined(__Bitrig__)
 
 #define alt_arc4random() arc4random()
-#define alt_arc4random_stir() arc4random_stir()
-#define alt_arc4random_addrandom(A, B) arc4random_addrandom(A, B)
+#ifdef HAVE_ARC4RANDOM_STIR
+# define alt_arc4random_stir() arc4random_stir()
+#else
+# define alt_arc4random_stir() (void) 0
+#endif
+#ifdef HAVE_ARC4RANDOM_ADDRANDOM
+# define alt_arc4random_addrandom(A, B) arc4random_addrandom(A, B)
+#else
+# define alt_arc4random_addrandom(A, B) (void) 0
+#endif
 #define alt_arc4random_uniform(A) arc4random_uniform(A)
 #define alt_arc4random_buf(A, B) arc4random_buf(A, B)
 
