76be2fcb7e7f22ff8f05302e1aebfed14dc7b8ef
Frank Denis <github@pureftpd.org>
2015-02-21 14:45:58 +0100

    Remove horrible TLS cruft

--- src/tls.c
+++ src/tls.c
@@ -47,120 +47,6 @@ static void tls_error(const int line, int err)
     _EXIT(EXIT_FAILURE);
 }
 
-DH *get_dh512(void)
-{
-    static unsigned char dh512_p[]={
-        0xF5,0x2A,0xFF,0x3C,0xE1,0xB1,0x29,0x40,0x18,0x11,0x8D,0x7C,
-        0x84,0xA7,0x0A,0x72,0xD6,0x86,0xC4,0x03,0x19,0xC8,0x07,0x29,
-        0x7A,0xCA,0x95,0x0C,0xD9,0x96,0x9F,0xAB,0xD0,0x0A,0x50,0x9B,
-        0x02,0x46,0xD3,0x08,0x3D,0x66,0xA4,0x5D,0x41,0x9F,0x9C,0x7C,
-        0xBD,0x89,0x4B,0x22,0x19,0x26,0xBA,0xAB,0xA2,0x5E,0xC3,0x55,
-        0xE9,0x2A,0x05,0x5F,
-    };
-    static unsigned char dh512_g[]={
-        0x02,
-    };
-    DH *dh;
-
-    if ((dh=DH_new()) == NULL) return(NULL);
-    dh->p=BN_bin2bn(dh512_p,sizeof(dh512_p),NULL);
-    dh->g=BN_bin2bn(dh512_g,sizeof(dh512_g),NULL);
-    if ((dh->p == NULL) || (dh->g == NULL))
-    { DH_free(dh); return(NULL); }
-    return(dh);
-}
-
-DH *get_dh1024(void)
-{
-    static unsigned char dh1024_p[]={
-        0xF4,0x88,0xFD,0x58,0x4E,0x49,0xDB,0xCD,0x20,0xB4,0x9D,0xE4,
-        0x91,0x07,0x36,0x6B,0x33,0x6C,0x38,0x0D,0x45,0x1D,0x0F,0x7C,
-        0x88,0xB3,0x1C,0x7C,0x5B,0x2D,0x8E,0xF6,0xF3,0xC9,0x23,0xC0,
-        0x43,0xF0,0xA5,0x5B,0x18,0x8D,0x8E,0xBB,0x55,0x8C,0xB8,0x5D,
-        0x38,0xD3,0x34,0xFD,0x7C,0x17,0x57,0x43,0xA3,0x1D,0x18,0x6C,
-        0xDE,0x33,0x21,0x2C,0xB5,0x2A,0xFF,0x3C,0xE1,0xB1,0x29,0x40,
-        0x18,0x11,0x8D,0x7C,0x84,0xA7,0x0A,0x72,0xD6,0x86,0xC4,0x03,
-        0x19,0xC8,0x07,0x29,0x7A,0xCA,0x95,0x0C,0xD9,0x96,0x9F,0xAB,
-        0xD0,0x0A,0x50,0x9B,0x02,0x46,0xD3,0x08,0x3D,0x66,0xA4,0x5D,
-        0x41,0x9F,0x9C,0x7C,0xBD,0x89,0x4B,0x22,0x19,0x26,0xBA,0xAB,
-        0xA2,0x5E,0xC3,0x55,0xE9,0x2F,0x78,0xC7,
-    };
-    static unsigned char dh1024_g[]={
-        0x02,
-    };
-    DH *dh;
-
-    if ((dh=DH_new()) == NULL) return(NULL);
-    dh->p=BN_bin2bn(dh1024_p,sizeof(dh1024_p),NULL);
-    dh->g=BN_bin2bn(dh1024_g,sizeof(dh1024_g),NULL);
-    if ((dh->p == NULL) || (dh->g == NULL))
-    { DH_free(dh); return(NULL); }
-    return(dh);
-}
-
-static DH *cb_tmp_dh(SSL * const ctx, const int for_export,
-                     const int key_length)
-{
-    (void) ctx;
-    if (for_export == 0 || key_length >= 1024) {
-        return get_dh1024();
-    }
-    return get_dh512();
-}
-
-static int tls_init_diffie(void)
-{
-    DH *dh = NULL;
-    BIO *bio = NULL;
-    int ret = 0;
-
-    if ((bio = BIO_new_file(TLS_CERTIFICATE_FILE, "r")) == NULL) {
-        logfile(LOG_ERR, "SSL/TLS: Can't read [%s]",
-                TLS_CERTIFICATE_FILE);
-        ret = -1;
-        goto end;
-    }
-    if ((dh = PEM_read_bio_DHparams(bio, NULL, NULL
-# if OPENSSL_VERSION_NUMBER >= 0x00904000L
-                                    , NULL
-# endif
-                                    )) == NULL) {
-        ret = 1;
-        goto end;
-    }
-    if (SSL_CTX_set_tmp_dh(tls_ctx, dh) != 1) {
-        logfile(LOG_ERR, "SSL/TLS: Can't set ephemeral keys");
-        ret = -1;
-    }
-    end:
-    if (dh != NULL) {
-        DH_free(dh);
-    }
-    if (bio != NULL) {
-        BIO_free(bio);
-    }
-    if (ret != 0) {
-        SSL_CTX_set_tmp_dh_callback(tls_ctx, cb_tmp_dh);
-    }
-    return 0;
-}
-
-static RSA *get_rsa(const unsigned long key_length)
-{
-    return RSA_generate_key(key_length, RSA_F4, NULL, NULL);
-}
-
-static RSA *cb_tmp_rsa(SSL * const ctx,
-                       const int for_export, const int key_length)
-
-{
-    (void) ctx;
-    if (for_export == 0 || key_length >= 1024) {
-        return get_rsa(1024);
-    }
-    return get_rsa(512);
-}
-
 static void tls_init_cache(void)
 {
     static const char *tls_ctx_id = "pure-ftpd";
@@ -252,10 +138,12 @@ int tls_init_library(void)
     if (SSL_CTX_check_private_key(tls_ctx) != 1) {
         tls_error(__LINE__, 0);
     }
-    SSL_CTX_set_tmp_rsa_callback(tls_ctx, cb_tmp_rsa);
-    if (tls_init_diffie() < 0) {
-        tls_error(__LINE__, 0);
-    }
+# ifdef SSL_CTRL_SET_DH_AUTO
+    SSL_CTX_ctrl(tls_ctx, SSL_CTRL_SET_DH_AUTO, 1, NULL);
+# endif
+# ifdef SSL_CTRL_SET_ECDH_AUTO
+    SSL_CTX_ctrl(tls_ctx, SSL_CTRL_SET_ECDH_AUTO, 1, NULL)
+# endif
     tls_init_cache();
 # ifdef DISABLE_SSL_RENEGOTIATION
     SSL_CTX_set_info_callback(tls_ctx, ssl_info_cb);
@@ -263,7 +151,7 @@ int tls_init_library(void)
 
 # ifdef REQUIRE_VALID_CLIENT_CERTIFICATE
     SSL_CTX_set_verify(tls_ctx, SSL_VERIFY_FAIL_IF_NO_PEER_CERT |
-                       SSL_VERIFY_PEER | SSL_VERIFY_CLIENT_ONCE, NULL);
+                       SSL_VERIFY_PEER, NULL);
     if (SSL_CTX_load_verify_locations(tls_ctx,
                                       TLS_CERTIFICATE_FILE, NULL) != 1) {
         tls_error(__LINE__, 0);
