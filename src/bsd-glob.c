b5aa8ccf6197ae1c8167a80c885c87e019fd9d94
Frank Denis <github@pureftpd.org>
2015-12-30 16:59:07 +0100

    Remove NULL checks before free()

--- src/bsd-glob.c
+++ src/bsd-glob.c
@@ -858,18 +858,14 @@ globfree(glob_t *pglob)
     if (pglob->gl_pathv != NULL) {
         pp = pglob->gl_pathv + pglob->gl_offs;
         for (i = pglob->gl_pathc; i--; ++pp) {
-            if (*pp) {
-                free(*pp);
-            }
+            free(*pp);
         }
         free(pglob->gl_pathv);
         pglob->gl_pathv = NULL;
     }
     if (pglob->gl_statv != NULL) {
         for (i = 0; i < pglob->gl_pathc; i++) {
-            if (pglob->gl_statv[i] != NULL) {
-                free(pglob->gl_statv[i]);
-            }
+            free(pglob->gl_statv[i]);
         }
         free(pglob->gl_statv);
         pglob->gl_statv = NULL;
