53a987d03b2c0fe0f3c1b3c453b33f3cc6e87d91
Frank Denis <github@pureftpd.org>
2019-03-24 14:55:32 +0100

    Indent

--- src/globals.h
+++ src/globals.h
@@ -9,59 +9,67 @@
 # define GLOBAL(A, B) extern A
 #endif
 
-GLOBAL(char default_tz_for_putenv[], "TZ=UTC+00:00");                /* default value for TZ */
-GLOBAL0(unsigned long long downloaded);                /* bytes downloaded */
-GLOBAL0(unsigned long long uploaded);                /* bytes uploaded */
-GLOBAL0(signed char anon_only);         /* allows only anonymous connections */
-GLOBAL0(struct sockaddr_storage *trustedip);  /* IP address accepting non-anonymous connections */
+GLOBAL(char default_tz_for_putenv[], "TZ=UTC+00:00"); /* default value for TZ */
+GLOBAL0(unsigned long long downloaded);               /* bytes downloaded */
+GLOBAL0(unsigned long long uploaded);                 /* bytes uploaded */
+GLOBAL0(signed char anon_only); /* allows only anonymous connections */
+GLOBAL0(struct sockaddr_storage *trustedip); /* IP address accepting non-anonymous connections */
 GLOBAL0(volatile signed char logging);
+
 #ifdef THROTTLING
 GLOBAL0(unsigned long throttling_delay);
-GLOBAL0(signed char throttling);                /* 0=don't throttle 1=throttle anon 2=all */
+GLOBAL0(signed char throttling); /* 0=don't throttle 1=throttle anon 2=all */
 #endif
+
 GLOBAL0(unsigned long throttling_bandwidth_dl);
 GLOBAL0(unsigned long throttling_bandwidth_ul);
-GLOBAL0(signed char allowfxp);                    /* 0=no fxp 1=authenticated 2=everybody */
+GLOBAL0(signed char allowfxp); /* 0=no fxp 1=authenticated 2=everybody */
 GLOBAL0(signed char passive);
-GLOBAL(int clientfd, 0);                   /* command connection file descriptor */
-GLOBAL(int datafd, -1);                    /* data connection file descriptor */
-GLOBAL0(struct sockaddr_storage ctrlconn);    /* stdin/stdout, for using the same ip number */
-GLOBAL0(signed char v6ready);                    /* IPv6 supported or not */
-GLOBAL0(signed char no_ipv4);                    /* IPv4 disabled or not */
+GLOBAL(int clientfd, 0); /* command connection file descriptor */
+GLOBAL(int datafd, -1);  /* data connection file descriptor */
+GLOBAL0(struct sockaddr_storage
+            ctrlconn);        /* stdin/stdout, for using the same ip number */
+GLOBAL0(signed char v6ready); /* IPv6 supported or not */
+GLOBAL0(signed char no_ipv4); /* IPv4 disabled or not */
 GLOBAL(const size_t cmdsize, PATH_MAX + 16U);
-GLOBAL0(char cmd[PATH_MAX + 32U]);        /* command line - about 30 chars for command */
-GLOBAL0(char wd[PATH_MAX + 1U]);            /* current working directory */
-GLOBAL0(char *root_directory);                /* root directory, for chroot'd environments */
-GLOBAL0(signed char loggedin);                    /* != 0 if the user if logged in */
-GLOBAL0(char account[MAX_USER_LENGTH + 1U]);      /* user login */
+GLOBAL0(char cmd[PATH_MAX + 32U]);   /* command line - about 30 chars for command */
+GLOBAL0(char wd[PATH_MAX + 1U]); /* current working directory */
+GLOBAL0(char *root_directory);   /* root directory, for chroot'd environments */
+GLOBAL0(signed char loggedin);   /* != 0 if the user if logged in */
+GLOBAL0(char account[MAX_USER_LENGTH + 1U]); /* user login */
 GLOBAL0(char *renamefrom);
-GLOBAL0(in_port_t serverport);    /* local server port */
-GLOBAL0(signed char userchroot);                /* 0=don't chroot() by default for regular users 1=chroot except members of the trusted group 2=chroot everyone */
-GLOBAL0(signed char chrooted);                     /* if we already chroot()ed */
+GLOBAL0(in_port_t serverport);   /* local server port */
+GLOBAL0(signed char userchroot); /* 0=don't chroot() by default for regular
+                                    users 1=chroot except members of the trusted
+                                    group 2=chroot everyone */
+GLOBAL0(signed char chrooted);   /* if we already chroot()ed */
 GLOBAL0(uid_t chroot_trustedgid);
-GLOBAL0(signed char broken_client_compat);         /* don't enable workarounds by default */
-GLOBAL0(uid_t warez);                    /* don't guard against warez */
-GLOBAL0(signed char debug);                        /* don't give debug output */
-GLOBAL0(signed char guest);                        /* if non-zero, it's a guest user */
-GLOBAL0(uid_t useruid);                    /* smallest uid which can ftp */
-GLOBAL0(signed char candownload);                /* if non-zero, don't let the user download */
-GLOBAL0(double load);                    /* for reporting what the load was */
-GLOBAL(time_t noopidle, (time_t) - 1);    /* when we started to receive NOOP */
-GLOBAL(unsigned int firstport, 1024U);        /* first and last ports to use, if we're */
+GLOBAL0(signed char broken_client_compat); /* don't enable workarounds by default */
+GLOBAL0(uid_t warez);                  /* don't guard against warez */
+GLOBAL0(signed char debug);            /* don't give debug output */
+GLOBAL0(signed char guest);            /* if non-zero, it's a guest user */
+GLOBAL0(uid_t useruid);                /* smallest uid which can ftp */
+GLOBAL0(signed char candownload); /* if non-zero, don't let the user download */
+GLOBAL0(double load);             /* for reporting what the load was */
+GLOBAL(time_t noopidle, (time_t)-1); /* when we started to receive NOOP */
+GLOBAL(unsigned int firstport, 1024U); /* first and last ports to use, if we're */
 GLOBAL(unsigned int lastport, 65535U);
-GLOBAL(signed char dot_write_ok, 1);           /* -x option */
-GLOBAL(signed char dot_read_ok, 1);            /* -X option */
-GLOBAL0(signed char dot_read_anon_ok);            /* -z option */
+GLOBAL(signed char dot_write_ok, 1);   /* -x option */
+GLOBAL(signed char dot_read_ok, 1);    /* -X option */
+GLOBAL0(signed char dot_read_anon_ok); /* -z option */
+
 #ifndef DEFAULT_TO_BINARY_TYPE
-GLOBAL(signed char type, 1);                    /* type - 0 = error, 1 = ascii, 2 = binary */
+GLOBAL(signed char type, 1); /* type - 0 = error, 1 = ascii, 2 = binary */
 #else
 GLOBAL(signed char type, 2);
 #endif
+
 #ifdef RATIOS
 GLOBAL0(unsigned int ratio_upload);
 GLOBAL0(unsigned int ratio_download);
 GLOBAL0(signed char ratio_for_non_anon);
 #endif
+
 GLOBAL0(off_t restartat);
 GLOBAL(unsigned long int idletime, DEFAULT_IDLE);
 GLOBAL0(double idletime_noop);
@@ -79,17 +87,22 @@ GLOBAL0(const char *force_passive_ip_s);
 GLOBAL0(in_port_t peerdataport);
 GLOBAL0(double maxload);
 GLOBAL(unsigned int maxusers, DEFAULT_MAX_USERS);
+
 #ifdef PER_USER_LIMITS
 GLOBAL0(unsigned int per_user_max);
 GLOBAL0(unsigned int per_anon_max);
 #endif
+
 GLOBAL(volatile int xferfd, -1);
+
 #ifndef NO_STANDALONE
 GLOBAL0(unsigned int maxip);
 #endif
+
 #ifndef NO_INETD
 GLOBAL0(signed char standalone);
 #endif
+
 GLOBAL0(signed char epsv_all);
 GLOBAL0(double maxdiskusagepct);
 GLOBAL0(signed char disallow_passive);
@@ -142,13 +155,14 @@ GLOBAL(unsigned long long user_quota_size, ULONG_LONG_MAX);
 GLOBAL(unsigned long long user_quota_files, ULONG_LONG_MAX);
 #endif
 
-#define MONTHS_NAMES "Jan", "Feb", "Mar", "Apr", "May", "Jun", \
-                     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-GLOBAL(const char *months[12], { MONTHS_NAMES });
+#define MONTHS_NAMES                        \
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun", \
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+GLOBAL(const char *months[12], {MONTHS_NAMES});
 
 #ifdef WITH_ALTLOG
-# define WEEK_DAYS_NAMES "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
-GLOBAL(const char *week_days[7], { WEEK_DAYS_NAMES });
+#define WEEK_DAYS_NAMES "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+GLOBAL(const char *week_days[7], {WEEK_DAYS_NAMES});
 #endif
 
 GLOBAL0(AuthResult authresult);
@@ -174,13 +188,13 @@ GLOBAL0(const char *extcert_socketpath);
 GLOBAL0(char *atomic_prefix);
 
 #ifdef WITH_RFC2640
-GLOBAL(signed char utf8, 0);    /* 0: ascii 1: utf-8 */
+GLOBAL(signed char utf8, 0); /* 0: ascii 1: utf-8 */
 GLOBAL(char *charset_fs, NULL);
 GLOBAL(char *charset_client, NULL);
-GLOBAL(iconv_t iconv_fd_fs2client, (iconv_t) -1);
-GLOBAL(iconv_t iconv_fd_fs2utf8, (iconv_t) -1);
-GLOBAL(iconv_t iconv_fd_client2fs, (iconv_t) -1);
-GLOBAL(iconv_t iconv_fd_utf82fs, (iconv_t) -1);
+GLOBAL(iconv_t iconv_fd_fs2client, (iconv_t)-1);
+GLOBAL(iconv_t iconv_fd_fs2utf8, (iconv_t)-1);
+GLOBAL(iconv_t iconv_fd_client2fs, (iconv_t)-1);
+GLOBAL(iconv_t iconv_fd_utf82fs, (iconv_t)-1);
 #endif
 
 #ifndef WITH_TLS
