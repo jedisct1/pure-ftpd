fc11e5edd1e52c22cd3caaefbb904df32634077a
Frank Denis <github@pureftpd.org>
2022-12-31 14:13:00 +0100

    Happy new year!

--- puredb/src/puredb_write.h
+++ puredb/src/puredb_write.h
@@ -1,5 +1,5 @@
 
-/* (C)opyleft 2001-2022 Frank DENIS <j at pureftpd dot org> */
+/* (C)opyleft 2001-2023 Frank DENIS <j at pureftpd dot org> */
 
 #ifndef __PUREDB_WRITE_H__
 #define __PUREDB_WRITE_H__ 1
